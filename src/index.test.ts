dfead25121d359f052412315bc02b784d3ce3ef5
Basarat <basaratali@gmail.com>
2022-10-07 16:52:15 +1100

    first

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,8 @@
-test('it should pass', () => {
+import { naive } from '.';
 
+const n = 10;
+const result = [...Array(n)].map((_, i) => i);
+
+test('it should pass', () => {
+  expect(naive(n)).toEqual(result);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-async function main() {
-  
-}
+/** 
+ * Create an array of n numbers [0, n-1]
+ **/
 
-main();
+export function naive(n: number) {
+  const result: number[] = [];
+  for (let index = 0; index < n; index++) {
+    result.push(index);
+  }
+  return result;
+}
